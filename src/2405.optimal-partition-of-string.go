44fa00f0fcdb23b5f470670d2a935a3a7778c978
chenk <1510687833@qq.com>
2023-04-06 14:03:03 +0800

    feat: Problem 2405

---
+++ src/2405.optimal-partition-of-string.go
@@ -0,0 +1,24 @@
+package src
+
+/*
+ * @lc app=leetcode id=2405 lang=golang
+ *
+ * [2405] Optimal Partition of String
+ */
+
+// @lc code=start
+func partitionString(s string) int {
+	numList := make([]int, 26)
+	count := 1
+	for i := 0; i < len(s); i++ {
+		j := s[i]
+		if numList[j-'a'] == 1 {
+			numList = make([]int, 26)
+			count++
+		}
+		numList[j-'a'] = 1
+	}
+	return count
+}
+
+// @lc code=end
